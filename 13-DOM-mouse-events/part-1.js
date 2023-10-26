b1ecdcfa8820d99a198badac4c38d70b65989343
Mariia Shuryha <mshurig2003@gmail.com>
2023-10-27 01:22:17 +0300

    add practice files change colors

--- 13-DOM-mouse-events/part-1.js
+++ 13-DOM-mouse-events/part-1.js
@@ -11,16 +11,16 @@ const PHRASE = 'Слава Україні!';
 const title = document.createElement("h1");
 const btn = document.createElement("button");
 btn.innerText="Розфарбувати";
-titleArray = PHRASE.split("").map(elem => {
+let titleArray= PHRASE.split("").map(elem => {
   return`<span>${elem}</span>`
-});
+})
 title.innerHTML=titleArray.join("");
-// btn.addEventListener("click", () => {
+// btn.addEventListener("click", () =>{
 //   for(let span of title.children){
 //     span.style.color=getRandomColor();
 //   }
 // })
-document.body.addEventListener('mousemove', () => {
+document.body.addEventListener("mousemove", () =>{
   for(let span of title.children){
     span.style.color=getRandomColor();
   }
