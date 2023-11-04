821bbd4347ea2739f0b05df43dab30c82b56053f
Mariia Shuryha <mshurig2003@gmail.com>
2023-11-04 19:03:22 +0200

    add changes to file HW-6-Tabs

--- 13-DOM-mouse-events/6-HW-tabs slider/tabs/scripts/index.js
+++ 13-DOM-mouse-events/6-HW-tabs slider/tabs/scripts/index.js
@@ -1,47 +1,20 @@
 const tabsTitle = document.querySelectorAll('.tabs-title');
 const tabsContent = document.querySelectorAll('.tabs-content li');
 
-tabsTitle.forEach(el => el.addEventListener('click', event =>{
+console.log(tabsTitle);
+console.log(tabsContent);
+
+tabsTitle.forEach(el => el.addEventListener('click', event => {
     tabsTitle.forEach(elem => elem.classList.remove('active'));
     tabsContent.forEach(elem => {
         elem.classList.add('hidden-tab-content');
-        if (elem.dataset.tab===event.target.innerText){
+        if(elem.dataset.tab === event.target.innerText){
             elem.classList.remove('hidden-tab-content');
         }
-    })
+    }
+    );
     el.classList.add('active');
 }))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
