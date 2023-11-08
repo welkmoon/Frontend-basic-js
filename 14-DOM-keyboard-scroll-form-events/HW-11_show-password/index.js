ad6398042b7794ed6c6fc694b080c209ba42eca6
Mariia Shuryha <mshurig2003@gmail.com>
2023-11-08 16:30:38 +0200

    add new HW 14-DOM files

--- 14-DOM-keyboard-scroll-form-events/HW-11_show-password/index.js
+++ 14-DOM-keyboard-scroll-form-events/HW-11_show-password/index.js
@@ -3,33 +3,33 @@ const form = document.querySelector('.password-form');
 const error = document.createElement('span');
 
 icons.forEach(icon => {
-    icon.addEventListener('click', e =>{
-        const input = document.querySelector(`.${e.target.dataset.icon}`);
-
-        if (input.type === 'password'){
+    icon.addEventListener('click', (event)=>{
+        const input = document.querySelector(`.${event.target.dataset.icon}`);
+        // console.log(input)
+        if(input.type === 'password'){
             input.type = 'text';
-            e.target.setAttribute('class', 'fas fa-eye-slash icon-password')
+            event.target.setAttribute('class', 'fas fa-eye-slash icon-password')
         } else {
             input.type = 'password';
-            e.target.setAttribute('class', 'fas fa-eye icon-password')
+            event.target.setAttribute('class', 'fas fa-eye icon-password')
         }
+
     })
 })
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit',(event) => {
     event.preventDefault();
-
     const inputs = event.target.querySelectorAll('input');
-    // console.log(inputs);
     const values = {};
-    inputs.forEach((input,index) => {
+    inputs.forEach((input, index) =>{
         values[index] = input.value;
     })
+    // console.log(values)
     if(values[0] === values[1]){
-        alert('You are welcome');
-    } else {
+        alert('You are welcome')
+    } else{
         error.style.color = 'red';
-        error.innerHTML= `Потрібно ввести однакові значення`;
+        error.innerText=`Потрібно ввести однакові значення`;
         const inputError = document.querySelector('.password-repeat');
         inputError.after(error);
 
